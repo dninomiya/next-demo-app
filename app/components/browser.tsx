aff9d49237e0dc463e994dc5decec1a8498fe557
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-11-11 13:22:28 +0900

    fi

--- app/components/browser.tsx
+++ app/components/browser.tsx
@@ -13,9 +13,7 @@ export default function Browser({ children }: { children: React.ReactNode }) {
           <span className="w-3 h-3 rounded-full bg-[#FCB024]"></span>
           <span className="w-3 h-3 rounded-full bg-[#28C131]"></span>
         </div>
-        <div className="bg-white">
-          <Suspense fallback={null}>{children}</Suspense>
-        </div>
+        <div className="bg-white">{children}</div>
       </div>
     </div>
   );
